b0fcb2a3e01871f44d0fc3197e9fb8be43ee3072
Walid Omar <walidomar291@gmail.com>
2025-01-27 08:20:09 +0200

    FE - using ngFor and ngIf with observable

--- angular/src/app/modules/doctors/doctor-availability/doctor-availability.component.ts
+++ angular/src/app/modules/doctors/doctor-availability/doctor-availability.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule, OnDestroy } from '@angular/core';
 import { DoctorAvailabilityDto, DoctorsService } from '@proxy/doctors';
 import { ActivatedRoute, Router } from '@angular/router';
-import { DatePipe } from '@angular/common';
+import { DatePipe, CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-doctor-availability',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CommonModule],
   templateUrl: './doctor-availability.component.html',
   styleUrl: './doctor-availability.component.scss'
 })
 
-export class DoctorAvailabilityComponent implements OnInit{
+export class DoctorAvailabilityComponent implements OnInit, OnDestroy{
   id: number ;
   doctorName: string | null = null;
   doctorAvailabilities: DoctorAvailabilityDto[];
+
+  paramSubscription?: Subscription;
+  getAvailabilitySubscription?: Subscription;
+
 
   constructor(private route: ActivatedRoute,
     private doctorService: DoctorsService)
     {}
 
+  ngOnDestroy(): void {
+    this.paramSubscription?.unsubscribe();
+    this.getAvailabilitySubscription?.unsubscribe();
+  }
+
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe({
+    this.paramSubscription = this.route.paramMap.subscribe({
       next: (param) => {
         this.id = Number(param.get('id'));
       }
@@ -31,7 +41,7 @@ export class DoctorAvailabilityComponent implements OnInit{
 
     if(this.id)
     {
-      this.doctorService.getDoctorAvailabilityById(this.id).subscribe({
+      this.getAvailabilitySubscription = this.doctorService.getDoctorAvailabilityById(this.id).subscribe({
         next: (response) =>{
           this.doctorAvailabilities = response;
           this.doctorName = response[0].doctorName;
--- angular/src/app/modules/doctors/list-doctors/list-doctors.component.ts
+++ angular/src/app/modules/doctors/list-doctors/list-doctors.component.ts
@@ -2,27 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { DoctorDto } from '@proxy/booking-system/doctors';
 import { DoctorsService } from '@proxy/doctors';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-doctors',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './list-doctors.component.html',
   styleUrl: './list-doctors.component.scss'
 })
 
 
 export class ListDoctorsComponent implements OnInit{
-  doctors: DoctorDto[];
+  doctors$?: Observable<DoctorDto[]>;
 
   constructor(private doctorService: DoctorsService){
   }
 
   ngOnInit(): void {
-    this.doctorService.getAllDoctors().subscribe((response) => {
-      this.doctors = response;
-    });
+    this.doctors$ = this.doctorService.getAllDoctors();
   } 
 
 }
